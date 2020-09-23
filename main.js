cdf22f21ae63b3aad77a937f10e0ac63b2c982d9
Kerr <kerr.salabao@gmail.com>
2020-09-22 20:54:21 -0500

    Initial Files

---
+++ main.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Zhu Li is on!');
+});
+
+
+
+
+
+
+
+
+
+
+client.login('NzU4MTM4MjM2MTc3NzQzODgz.X2qlQQ.NJuGtxfVYwNSdLxmgAv554IY2hY')
