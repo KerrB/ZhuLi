776d863861f408e646922fe59df85fc7146bf34d
Kerr <kerr.salabao@gmail.com>
2020-09-22 21:45:27 -0500

    Figured out how to add a listener and responder

--- main.js
+++ main.js
@@ -7,12 +7,21 @@ client.once('ready', () => {
 });
 
 
+//client.on is waiting for the client to be on
+//How does a bot stay on?
+//TODO: Figure out how to run functions outside of main. Do we just reference them?
+client.on('message', message => {
+
+    if(message.content == '!ping')
+    {
+        message.channel.send('STFU');
+    }
+
+});
 
 
 
 
 
-
-
-
-client.login('NzU4MTM4MjM2MTc3NzQzODgz.X2qlQQ.NJuGtxfVYwNSdLxmgAv554IY2hY')
+//this has to be the end
+client.login('NzU4MTM4MjM2MTc3NzQzODgz.X2qlQQ.ZvgMes9fEKdqwlWNr14QypXUl6c')
